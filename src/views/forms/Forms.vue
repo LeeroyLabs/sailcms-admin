c5c5ff8dfbbfc778cd6c30d8eb60795dda52a8cb
Romuald Gaboriau <73833432+Rom1023@users.noreply.github.com>
2023-08-24 10:28:01 -0400

    feat(tasks): wip

--- src/views/forms/Forms.vue
+++ src/views/forms/Forms.vue
@@ -1,7 +1,20 @@
 <template>
     <div>FORMS</div>
+    <Listing
+        ref="listing"
+        :loading="false"
+        :multi="true"
+        :files="[]"
+        mode="image"
+        display-mode="grid"
+    />
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import Listing from "@/components/globals/assetmanager/Listing.vue";
+
+const listing = ref(null);
+</script>
 
 <style lang="scss" scoped></style>
